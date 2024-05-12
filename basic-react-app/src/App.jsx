e0173b1abf67d8bd0da3710466e99200331bf90d
cayan701 <cayan701@gmail.com>
2024-05-12 07:23:34 +0530

    Add new files

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css';
 
-const description = ['fundatmental, core, random'];
+const reactDescriptions = ['fundatmental', 'core', 'random'];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
+function Header() {
+  const description = reactDescriptions[getRandomInt(2)];
+
+  return (
+    <header>
+      <img src="src/assets/react.svg" alt="hey" />
+      <h1>New Website</h1>
+      <p> {description} This is a random website</p>
+    </header>
+  );
+}
+
 function App() {
-  return <Title/>
+  return <Header />
 }
 
 export default App
